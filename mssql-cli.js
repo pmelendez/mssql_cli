d311fa43fc24850569f6d536c3b549e2803adefa
Pedro Melendez <pedro.melendez@shomi.com>
2016-02-10 19:03:08 -0500

    skeleton

--- mssql-cli.js
+++ mssql-cli.js
@@ -1,3 +1,24 @@
 #!/usr/local/bin/node
+var mssql = require('./mssql-promise')
+var colors = require('colors')
+var console_highlight = require('console-highlight')
 
-console.log("First commit")
+console.log("First commit".green)
+
+console.log(mssql.query("hello").gray)
+
+var program = require('commander');
+
+program
+.version('0.0.1')
+.option('-p, --peppers', 'Add peppers')
+.option('-P, --pineapple', 'Add pineapple')
+.option('-b, --bbq-sauce', 'Add bbq sauce')
+.option('-c, --cheese [type]', 'Add the specified type of cheese [marble]', 'marble')
+.parse(process.argv);
+
+console.log('you ordered a pizza with:');
+if (program.peppers) console.log('  - peppers');
+if (program.pineapple) console.log('  - pineapple');
+if (program.bbqSauce) console.log('  - bbq');
+console.log('  - %s cheese', program.cheese);
---
+++ mssql-promise.js
@@ -0,0 +1,12 @@
+var bluebird = require('bluebird')
+var mssql = require('mssql')
+
+exports.query = function (strQuery)
+{
+    return strQuery;
+}
+
+var getConnection = function()
+{
+
+}
